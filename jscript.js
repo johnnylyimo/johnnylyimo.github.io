069759858926492d49db14b8df2567b2d990e263
johnnylyimo <johnnielyimo@gmail.com>
2019-03-10 00:57:25 +0300

    update  files

--- jscript.js
+++ jscript.js
@@ -1,5 +1,19 @@
 // JavaScript codes for Day 3
 
+// Function defination
+function add(num1, num2){
+    return num1 + num2;
+}
+function minus(num1, num2){
+    return num1 - num2;
+}
+function product(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+
 // Access DOM elements
 const button = document.getElementById("btn");
 const calcRadioBtns = document.getElementsByName('operation');
@@ -7,6 +21,7 @@ const calcResult = document.getElementById('calcAns');
 const radioAdd = document.getElementById('radioAdd');
 let textno1 = document.getElementById("txtboxnum1");
 const operartion_Btns =document.getElementById('radioBtnOper');
+const oper_Error = document.getElementById('operError');
 
 
 
@@ -19,12 +34,16 @@ const operartion_Btns =document.getElementById('radioBtnOper');
 
     // Validate user input
   if (textno1.value.length === 0) {
-        alert("Either First Number is missing! \nor Invalid input(Letters/symbols not allowed!)");
+        // alert("Either First Number is missing! \nor Invalid input(Letters/symbols not allowed!)");
+        oper_Error.classList.remove('d-none');
+        oper_Error.classList.add('d-block');
         textno1.focus();
     }
         
     else if(textno2.value.length == 0){
         alert("Either Second Number is missing! \nor Invalid input(Letters/symbols not allowed!)");
+        oper_Error.classList.remove('d-none');
+        oper_Error.classList.add('d-block');
         textno2.focus();
     } 
   
@@ -53,26 +72,26 @@ const operartion_Btns =document.getElementById('radioBtnOper');
  
             console.log('computation begin');
             if (calcRadioBtns[index].value === '-'){
-              calcResult.textContent = ' = ' + (Number(textno1.value - textno2.value));
+              calcResult.textContent = ' = ' + minus(Number(textno1.value), Number(textno2.value));
               oper_Error.classList.remove('d-block')
               oper_Error.classList.add('d-none')
               button.textContent = "Clear";
             }
             else if(calcRadioBtns[index].value === '+'){
-              calcResult.textContent = ' = ' + (Number(textno1.value) + Number(textno2.value));
+              calcResult.textContent = ' = ' + add(Number(textno1.value), Number(textno2.value));
               oper_Error.classList.remove('d-block')
               oper_Error.classList.add('d-none')
               button.textContent = "Clear";
               
             } 
             else if(calcRadioBtns[index].value === '*'){
-              calcResult.textContent = ' = ' + (Number(textno1.value) * Number(textno2.value));
+              calcResult.textContent = ' = ' + product(Number(textno1.value), Number(textno2.value));
               oper_Error.classList.remove('d-block')
               oper_Error.classList.add('d-none')
               button.textContent = "Clear";
             }
             else if(calcRadioBtns[index].value === '/'){
-              calcResult.textContent = ' = ' + (Number(textno1.value) / Number(textno2.value));
+              calcResult.textContent = ' = ' + divide(Number(textno1.value), Number(textno2.value));
               oper_Error.classList.remove('d-block')
               oper_Error.classList.add('d-none')
               button.textContent = "Clear";
